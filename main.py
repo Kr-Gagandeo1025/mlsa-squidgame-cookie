5cb64d6497062c9b25d445a403f353e9e889423f
Kr-Gagandeo1025 <kumargagandeo9@gmail.com>
2023-04-16 21:47:09 +0530

    second commit

--- main.py
+++ main.py
@@ -26,12 +26,22 @@ NotWon =True
 
 #GAME LOGIC
 cam = cv2.VideoCapture(0)
+cookie_img = cv2.imread('img/sqr(2).png')
+size = 200
+cookie_img = cv2.resize(cookie_img, (size, size))
+  
+# Create a mask of logo
+img2gray = cv2.cvtColor(cookie_img, cv2.COLOR_BGR2GRAY)
+ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
 detector = HD.HandDetector()
 mnlst=[]
 i = 0
 while True:
-    success,img=cam.read()
+    ret, img = cam.read()
     imgflip = cv2.flip(img,1)
+    roi = imgflip[-size-200:-200, -size-200:-200]
+    roi[np.where(mask)] = 1
+    roi += cookie_img
     img = detector.findHands(imgflip)
     lnList = detector.findPosition(imgflip)
 
@@ -45,11 +55,10 @@ while True:
             cv2.circle(imgflip,(j[0],j[1]),4,(255,0,0),-1)
         # mX = np.interp(index1Y, (0,640),(0,1920))
         # mY = np.interp(index1X, (0,480),(0,1080))
-    cv2.imshow("Image",imgflip)
-    cookieimg = cv2.imread('img/sqr(2).png')
-    overlay = cookieimg.copy()
-    alpha = 0.4  # Transparency factor.
-    cookieimg_new = cv2.addWeighted(overlay, alpha, cookieimg, 1 - alpha, 0)
-    cv2.imshow('cookie',cv2.resize(cookieimg_new, (0, 0), fx=0.69, fy=0.69))
+    # cv2.imshow("Image",imgflip)
+    # alpha = 0.4  # Transparency factor.
+    # cookieimg_new = cv2.addWeighted(imgflip[150:250,150:250,:], alpha, roi, 1 - alpha, 0)
+    # imgflip[150:250,150:250] = cookieimg_new 
+    cv2.imshow('game',cv2.resize(imgflip,(0,0),fx=0.69,fy=0.69))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
